6121d3103a7bcc798d8010c5c648e5da86ed09f9
Sinh Nguyen <snguyen0219@gmail.com>
2015-09-14 13:26:31 -0500

    Takes out static and public that was not needed

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,18 +1,9 @@
-package edu.jsu.mcis;
 
 public class TicTacToe {
-	//private static Scanner scan = new Scanner(System.in);
-	//all these below were static
-	public static int row, col;
-	public static char[][] board = new char[3][3];
-	public static char who = 'X';
-	public static int turns = 0;
-	private static String wonHow = "  ";
-	/*	
-	public static void main(String[] args) {
-	NewGame();	
-	}
-	*/	
+	private char[][] board = new char[3][3];
+	private char who = 'X';
+	private int turns = 0;
+	private String wonHow = "  ";
 	
 	public TicTacToe(){
 		turns = 0;
@@ -22,65 +13,7 @@ public class TicTacToe {
 			}
 		}
 	}
-	
-	/*private void NewGame(){
-		//the Constructor "TicTacToe" already does this
-		//ClearBoard();
-		PrintBoard();
-		while (StillRunning()){
-		Winner();
-		//out.println();
-		out.print("Player " + who + ",enter a row: ");
-		row = scan.nextInt()-1;
-		out.print("Player " + who + ",enter a col: ");
-		col = scan.nextInt()-1;
-		Mark(row, col);
-		PrintBoard();
-		GetWinner();
-		Winner();
-		}
-	}
-	*/
-	public void ClearBoard(){
-		turns = 0;
-		for (int i = 0; i < board.length; i++){
-			for (int j = 0; j < board.length; j++){
-				board[i][j] = '_';
-			}
-		}
-	}
-	
-	/*private void PrintBoard(){
-		for (int i = 0; i < 3; i++){
-			//out.println();
-			for (int j = 0; j < 3; j++){
-				{if (board[i][j] == '_')
-						out.print('_');
-				else 
-					out.print(board[i][j]);
-				}
-			}
-		}
-	}	
-	*/
-	public boolean IsBoardEmpty(){
-		boolean empty = true;
-		if (board[0][0] == board[0][1] 
-				&& board[0][0] == board[0][2]
-						&& board[0][2] == board[1][0] 
-								&&	board[1][0] == board[1][1]
-										& board[1][0] == board[1][2]
-												&& board[1][2] == board[2][0]
-														&& board[2][0] == board[2][1]
-																&& board[2][0] == board[2][2]
-																		&& board[2][2] == '_'){
-			empty = true;
-		}else {
-				empty = false;
-		}
-		return empty;
-		}
-	
+
 	public void Mark( int rMove, int cMove){
 		if (board[rMove][cMove] == '_'){
 			board[rMove][cMove] = who;
@@ -92,10 +25,6 @@ public class TicTacToe {
 			 turns++;
 		}
 
-		else {
-			UnableToMark();
-			//out.println("\n\n" + UnableToMark());
-		}
 	}
 
 	public String GetMark(int rMove, int cMove){
@@ -104,152 +33,31 @@ public class TicTacToe {
 		return location;
 	}
 	
-	/*
-
-	public static String CheckLocation(int rMove, int cMove){
-		String character = Character.toString(board[rMove][cMove]);
-		String location =  character;
-		return location;
-	}
-	*/
-	
-	public String UnableToMark(){
-		return "This tile has already been taken.";		
-	}
-	
-	public boolean StillRunning(){
-		if(turns == 9)
-			return false;
-		 else 
-			 return true;
-		}
-	
-	private boolean HorizontalWin(){
-		if (board[0][0] == board[0][1] && 
-				board[0][0] == board[0][2] &&
-				board[0][0] != '_')
-			return true;
-		if (board[1][0] == board[1][1] && 
-				board[1][0] == board[1][2] &&
-				board[1][0] != '_')
-			return true;
-		if (board[2][0] == board[2][1] && 
-				board[2][0] == board[2][2] &&
-				board[2][0] != '_'){
-			return true;
-		}
-		else 
-			return false;
-	}
-	
-	private boolean VerticalWin(){
-		if (board[0][0] == board[1][0] && 
-				board[0][0] == board[2][0] &&
-				board[0][0] != '_' )
-			return true;
-		if (board[0][1] == board[1][1] && 
-				board[0][1] == board[2][1] &&
-				board[0][1] != '_')
-			return true;
-		if (board[0][2] == board[1][2] && 
-				board[0][2] == board[2][2] &&
-				board[0][2] != '_')
-			return true;
-		else 
-			return false;
-	}
-	
-	private boolean DiagonalWin(){
-		if (board[0][0] == board[1][1] && 
-				board[0][0] == board[2][2] &&
-				board[0][0] != '_')
-			return true;
-		if (board[0][2] == board[1][1] && 
-				board[0][2] == board[2][0] &&
-				board[0][2] != '_')
-			return true;
-		else 
-			return false;
-	}
-	
-	public String GetWinner(){
-		if (turns != 10){
-				if (DiagonalWin()){
-					wonHow = "Diagonal";
-				//	turns = 10;
-				}
-				else if (HorizontalWin()){
-					wonHow = "Horizontal";
-				//	turns = 10;
-				}
-				else if (VerticalWin()){
-					wonHow = "Vertical";
-					//turns = 10;
-				}	
-				else {
-					return "Running";
-				}
-				
-		}
-		else if (turns == 10){
-			wonHow = "Tie";
-		}
-		return Winner();
-		
-		}
-	
-	public String Winner(){
-		String winner = "";
-		if (wonHow == "Diagonal"){
-		 if (who == 'X'){
-				who = 'O';
-		 }
-		 else {
-				who = 'X';}
+	public String whoIsWinner(){
+		if ((board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] != '_')
+			|| (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] != '_')
+			|| (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] != '_')
+			|| (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] != '_' )
+			|| (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] != '_')
+			|| (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] != '_')
+			|| (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != '_')
+			|| (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != '_'))
+		{
 			if (who == 'X'){
-				winner = "X";
-			}else {
-				winner = "O";
+			wonHow = "O";
+			}
+			else {
+				wonHow = "X";
 			}
 		}
-		else if (wonHow == "Horizontal"){
-			 if (who == 'X'){
-					who = 'O';
-			 }
-			 else {
-					who = 'X';}
-				if (who == 'X'){
-					winner = "X";
-				}else {
-					winner = "O";
-				}
-			}
-		else if (wonHow == "Vertical"){
-			 if (who == 'X'){
-					who = 'O';
-			 }
-			 else {
-					who = 'X';}
-				if (who == 'X'){
-					winner = "X";
-				}else {
-					winner = "O";
-				}
-			}else if (wonHow == "Tie"){
-				 if (who == 'X'){
-						who = 'O';
-				 }
-				 else {
-					 who = 'O';
-				 }
-				winner = "TIE";
+		else if ((turns == 9 && wonHow != "X") || (turns == 9 && wonHow != "O")) {
+			wonHow = "TIE" ;
 		}
-		
-
-		
-		return winner;
-	}
-
+		else {
+			return "";
+		}
+		return wonHow;
+}
 }
 
 
--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -1,4 +1,4 @@
-package edu.jsu.mcis;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JButton;
@@ -6,37 +6,37 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
-	
-@SuppressWarnings("serial")
+//why is this giving me this error?	
+//@SuppressWarnings("serial")
 public class TicTacToeGUI extends JFrame{
     
     private static  JButton[][] buttons = new JButton[3][3];
     
-    static TicTacToe ttt = new TicTacToe();
-	
+    static TicTacToe ttt = new TicTacToe();	
     
 	public static void main(String[] args) {
-	    JFrame mainBoard = new JFrame("Tic-Tac-Toe");
-        mainBoard.setSize(600,600);
-        mainBoard.setLayout(new GridLayout(3,3));
-        mainBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    JFrame guiBoard = new JFrame("Tic Tac Toe");
+        guiBoard.setSize(700,700);
+        guiBoard.setLayout(new GridLayout(3,3));
+        guiBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
             	buttons[i][j] = new JButton("");
                 buttons[i][j].setName("Location" + i + j);
                 buttons[i][j].addActionListener(new ButtonListener(i, j));
-                buttons[i][j].setFont(new Font("", Font.PLAIN, 72));
-                mainBoard.add(buttons[i][j]);
+                buttons[i][j].setFont(new Font("", Font.PLAIN, 108));
+                guiBoard.add(buttons[i][j]);
 
             }
 
         }
-        mainBoard.setVisible(true);
+        guiBoard.setVisible(true);
 	}
 
-	public void setupBoard(){
+	/*public void setupBoard(){
 
 	}
+	*/
 
 	private static class ButtonListener implements ActionListener {
 	    private int row;
@@ -56,8 +56,10 @@ public class TicTacToeGUI extends JFrame{
 	private static void pressButton (int row, int col) {
 	    ttt.Mark(row, col);
 	    buttons[row][col].setText(ttt.GetMark(row, col));
-	    if (ttt.GetWinner().equals("X")) {
-	        int delay = 1000;
+	    if (ttt.whoIsWinner().equals("X")) {
+	    	//trying 500 ms to see if this helps
+	        //int delay = 1000;
+	        int delay = 300;
 	        ActionListener taskPerformer = new ActionListener() {
 	            public void actionPerformed(ActionEvent evt) {
 	            JOptionPane.showMessageDialog(null, "The winner is X", "Game Over", JOptionPane.INFORMATION_MESSAGE);
@@ -67,8 +69,10 @@ public class TicTacToeGUI extends JFrame{
 	        myTimer.setRepeats(false);
 	        myTimer.start();
 	    }
-	    else if (ttt.GetWinner().equals("O")) {
-	        int delay = 1000;
+	    else if (ttt.whoIsWinner().equals("O")) {
+	    	//trying 500 ms to see if this helps
+	    	//int delay = 1000;
+	    	int delay = 300;
 	        ActionListener taskPerformer = new ActionListener() {
 	            public void actionPerformed(ActionEvent evt) {
 	            JOptionPane.showMessageDialog(null, "The winner is O", "Game Over", JOptionPane.INFORMATION_MESSAGE);
@@ -78,8 +82,10 @@ public class TicTacToeGUI extends JFrame{
 	        myTimer.setRepeats(false);
 	        myTimer.start();
 	    }
-	    else if (ttt.GetWinner().equals("TIE")) {
-	        int delay = 1000;
+	    else if (ttt.whoIsWinner().equals("TIE")) {
+	    	//trying 500 ms to see if this helps
+	    	//int delay = 1000;
+	        int delay = 300;
 	        ActionListener taskPerformer = new ActionListener() {
 	            public void actionPerformed(ActionEvent evt) {
 	            JOptionPane.showMessageDialog(null, "The winner is TIE", "Game Over", JOptionPane.INFORMATION_MESSAGE);
