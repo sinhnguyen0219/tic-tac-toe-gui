4e6e5b56159678191980132e7364fddd73158ce9
Sinh Nguyen <snguyen0219@gmail.com>
2015-09-16 17:10:14 -0500

    Makes more things private and runs with no errors

--- TTT with acceptance tests working Tests/TicTacToeGUI.java
+++ TTT with acceptance tests working Tests/TicTacToeGUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class TicTacToeGUI extends JFrame{
     
-    private JButton[][] buttons = new JButton[3][3];
+    private JButton[][] button = new JButton[3][3];
     
     TicTacToe ttt = new TicTacToe();	
 	
@@ -23,34 +23,35 @@ public class TicTacToeGUI extends JFrame{
     guiBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-        	buttons[i][j] = new JButton("");
-            buttons[i][j].setName("Location" + i + j);
-            buttons[i][j].addActionListener(new ButtonListener(i, j));
-            buttons[i][j].setFont(new Font("", Font.PLAIN, 140));
-            guiBoard.add(buttons[i][j]);
+        	button[i][j] = new JButton("");
+            button[i][j].setName("Location" + i + j);
+            button[i][j].addActionListener(new ButtonListener(i, j));
+            button[i][j].setFont(new Font("", Font.PLAIN, 140));
+            guiBoard.add(button[i][j]);
 
         }
 
     }
     guiBoard.setVisible(true);
 	}
+	
 	private class ButtonListener implements ActionListener {
 	    private int row;
 	    private int col;
     
-    public ButtonListener(int i, int j) {
-        row = i;
-        col = j;
-    }
+		private ButtonListener(int i, int j) {
+			row = i;
+			col = j;
+		}
     //ask about why making the class below private throws an error but still runs
-    public void actionPerformed(ActionEvent evt) {
-        pressButton(row, col);
-    }
+		public void actionPerformed(ActionEvent evt) {
+			pressButton(row, col);
+			}
 	}
 	
 	private void pressButton (int row, int col) {
 	    ttt.Mark(row, col);
-	    buttons[row][col].setText(ttt.GetMark(row, col));
+	    button[row][col].setText(ttt.GetMark(row, col));
 	    if (ttt.whoIsWinner().equals("X")) {
 	        int delay = 50;
 	        ActionListener taskPerformer = new ActionListener() {
